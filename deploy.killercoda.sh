8fa523e0479b86ed22145ad4f8014fd3058202a0
Alexander Aguilar <alexander.aguilar.torrico@gmail.com>
2023-11-22 10:07:59 -0400

    fix killercoda deploy

--- deploy.killercoda.sh
+++ deploy.killercoda.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-
+  
 echo "Init creating ssh key to verify conectivity...."
 
 sudo apt install ansible git
 
-ssh-keygen -q -b 2048 -t rsa -N "" -C "creating SSH" -f KEY-UBUNTU creates="KEY-UBUNTU"
+ssh-keygen -q -b 2048 -t rsa -N "" -C "creating SSH" -f KEY-UBUNTU
 
 chmod 400 KEY-UBUNTU
 
-cat KEY-UBUNTU.pub >> ~/.ssh/authorized_keys 
+cat KEY-UBUNTU.pub >> ~/.ssh/authorized_keys
 
 git clone https://github.com/failoverbodev/failoverbo.nextcloud.git
 
-ansible --version
-
-ansible -i host -m ping all
+ansible -i hosts -m ping all
