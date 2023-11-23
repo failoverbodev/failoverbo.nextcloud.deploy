387c9646d3a6ba373eb9a3c4fee4f490c5527822
Alexander Aguilar <alexander.aguilar.torrico@gmail.com>
2023-11-23 10:22:58 -0400

    fix script killercoda automation

--- deploy.killercoda.sh
+++ deploy.killercoda.sh
@@ -2,11 +2,11 @@
   
 echo "Init creating ssh key to verify conectivity...."
 
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 
-sudo apt install ansible git net-tools
+sudo apt install -y ansible git net-tools
 
 ssh-keygen -q -b 2048 -t rsa -N "" -C "creating SSH" -f KEY-UBUNTU
 
@@ -17,3 +17,7 @@ cat KEY-UBUNTU.pub >> ~/.ssh/authorized_keys
 git clone https://github.com/failoverbodev/failoverbo.nextcloud.git
 
 ansible -i hosts -m ping all
+
+ansible-playbook -i hosts deploy.yml -v
+
+sudo -u www-data php /var/www/nextcloud/occ config:system:set trusted_domains 2 --value=$(cat /etc/killercoda/host | sed 's/PORT/80/g' | sed 's/https:\/\///g')
